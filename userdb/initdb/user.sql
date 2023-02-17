b31da15b2ed374f030647b930934bd20e7888259
kaminakaya.ken.7s5 <kaminakaya.ken.7s5@jp.nssol.nipponsteel.com>
2023-02-17 14:52:17 +0900

    userdb修正

--- userdb/initdb/user.sql
+++ userdb/initdb/user.sql
@@ -1,13 +1,10 @@
-USE demo;
+USE user;
 
 DROP TABLE IF EXISTS user;
 CREATE TABLE user(
 	user_id VARCHAR(50) PRIMARY KEY,
 	password VARCHAR(100),
 	user_name VARCHAR(50),
-	birthday DATE,
-	age INT,
-	marriage BOOLEAN,
 	role VARCHAR(50)
 );
 
@@ -16,19 +13,13 @@ INSERT INTO user
   user_id,
   password,
   user_name,
-  birthday,
-  age,
-  marriage,
   role
 )
 VALUES
 (
   'test@example.com',
-  '$2a$10$xRTXvpMWly0oGiu65WZlm.3YL95LGVV2ASFjDhe6WF4.Qji1huIPa',
+  'password',
   'test',
-  '1990-01-01',
-  28,
-  false,
   'ROLE_ADMIN'
 );
 
@@ -37,19 +28,13 @@ INSERT INTO user
   user_id,
   password,
   user_name,
-  birthday,
-  age,
-  marriage,
   role
 )
 VALUES
 (
   'admin@example.com',
-  '$2a$10$xRTXvpMWly0oGiu65WZlm.3YL95LGVV2ASFjDhe6WF4.Qji1huIPa',
+  'password',
   'admin',
-  '1995-03-03',
-  35,
-  true,
   'ROLE_ADMIN'
 );
 
@@ -58,18 +43,12 @@ INSERT INTO user
   user_id,
   password,
   user_name,
-  birthday,
-  age,
-  marriage,
   role
 )
 VALUES
 (
   'general@example.com',
-  '$2a$10$xRTXvpMWly0oGiu65WZlm.3YL95LGVV2ASFjDhe6WF4.Qji1huIPa',
+  'password',
   'general',
-  '2000-05-05',
-  21,
-  false,
   'ROLE_GENERAL'
 );
