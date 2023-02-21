3fa36c9023a7329a81f45134be33acbd4a19a24b
kaminakaya.ken.7s5 <kaminakaya.ken.7s5@jp.nssol.nipponsteel.com>
2023-02-21 15:09:21 +0900

    item apiのログを出力

---
+++ item/src/main/java/com/example/item/LogAspect.java
@@ -0,0 +1,54 @@
+package com.example.item;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LogAspect {
+  private static final Logger logger = LogManager.getLogger(LogAspect.class);
+
+  @Around("execution(* *..*.*Controller.*(..))")
+  public Object startLog(ProceedingJoinPoint jp) throws Throwable {
+
+    logger.info("メソッド開始：" + jp.getSignature());
+
+    try {
+      Object result = jp.proceed();
+      logger.info("メソッド終了：" + jp.getSignature());
+
+      return result;
+
+    } catch (Exception e) {
+
+      logger.error("メソッド異常終了：" + jp.getSignature());
+      logger.error("system error", e);
+      throw e;
+    }
+  }
+
+  // UserDaoクラスのログ出力
+  @Around("execution(* *..*.*UserMapper.*(..))")
+  public Object daoLog(ProceedingJoinPoint jp) throws Throwable {
+
+    logger.info("メソッド開始：" + jp.getSignature());
+
+    try {
+      Object result = jp.proceed();
+      logger.info("メソッド終了：" + jp.getSignature());
+
+      return result;
+
+    } catch (Exception e) {
+
+      logger.error("メソッド異常終了：" + jp.getSignature());
+      logger.error("system error", e);
+      throw e;
+
+    }
+  }
+}
